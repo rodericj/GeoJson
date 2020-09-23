7af565085dd8e6dbf88c12f0ffed81139de9cec0
Roderic Campbell <roderic@gmail.com>
2020-09-23 08:23:36 -0700

    Initial Commit

---
+++ GeoJson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeoJson
+//
+//  Created by Roderic Campbell on 9/23/20.
+//  Copyright © 2020 Thumbworks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
